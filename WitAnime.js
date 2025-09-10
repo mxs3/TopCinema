5276af9b8d7afc2ed3d625a49dd2ce6eec7d300a
MxS3 <elsanea2019@gmail.com>
2025-09-10 08:29:13 +0300

    Update WitAnime.js

--- WitAnime.js
+++ WitAnime.js
@@ -185,3 +185,16 @@ async function extractEpisodes(url) {
     return JSON.stringify([]);
   }
 }
+
+// ===== سورا فيتش =====
+async function soraFetch(url, options = { headers: {}, method: 'GET', body: null }) {
+    try {
+        return await fetchv2(url, options.headers ?? {}, options.method ?? 'GET', options.body ?? null);
+    } catch (e) {
+        try {
+            return await fetch(url, options);
+        } catch (error) {
+            return null;
+        }
+    }
+}
