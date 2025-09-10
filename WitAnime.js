1a00ca20cf787c0c029c06123f097b76a26117f2
MxS3 <elsanea2019@gmail.com>
2025-09-10 07:36:07 +0300

    Update WitAnime.js

--- WitAnime.js
+++ WitAnime.js
@@ -1,3 +1,21 @@
+function decodeHTMLEntities(text) {
+    text = text.replace(/&#(\d+);/g, (match, dec) => String.fromCharCode(dec));
+
+    const entities = {
+        '&quot;': '"',
+        '&amp;': '&',
+        '&apos;': "'",
+        '&lt;': '<',
+        '&gt;': '>'
+    };
+
+    for (const entity in entities) {
+        text = text.replace(new RegExp(entity, 'g'), entities[entity]);
+    }
+
+    return text;
+}
+
 async function searchResults(keyword) {
   try {
     const url = `https://witanime.xyz/?search_param=animes&s=${encodeURIComponent(keyword)}`;
